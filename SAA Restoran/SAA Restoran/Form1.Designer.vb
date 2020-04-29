0ff7154f09e6dc39b2b13ac06cfd1ed5cefa3f1a
GTerrence <57930152+GTerrence@users.noreply.github.com>
2020-04-29 13:55:30 +0700

    Login

--- SAA Restoran/SAA Restoran/Form1.Designer.vb
+++ SAA Restoran/SAA Restoran/Form1.Designer.vb
@@ -22,10 +22,75 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TbUser = New System.Windows.Forms.TextBox()
+        Me.TbPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TbUser
+        '
+        Me.TbUser.Location = New System.Drawing.Point(439, 104)
+        Me.TbUser.Name = "TbUser"
+        Me.TbUser.Size = New System.Drawing.Size(130, 20)
+        Me.TbUser.TabIndex = 0
+        '
+        'TbPassword
+        '
+        Me.TbPassword.Location = New System.Drawing.Point(439, 143)
+        Me.TbPassword.Name = "TbPassword"
+        Me.TbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TbPassword.Size = New System.Drawing.Size(130, 20)
+        Me.TbPassword.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(355, 104)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(355, 143)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Location = New System.Drawing.Point(460, 199)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLogin.TabIndex = 4
+        Me.BtnLogin.Text = "Login"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TbPassword)
+        Me.Controls.Add(Me.TbUser)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TbUser As TextBox
+    Friend WithEvents TbPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnLogin As Button
 End Class
