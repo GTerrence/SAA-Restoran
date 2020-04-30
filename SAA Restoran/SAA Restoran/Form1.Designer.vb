5be98ec8fdcaf5dd4254d8294c2bd001218d90f6
hanjaya888 <57930023+hanjaya888@users.noreply.github.com>
2020-04-30 08:53:46 +0700

    coba hanjaya

--- SAA Restoran/SAA Restoran/Form1.Designer.vb
+++ SAA Restoran/SAA Restoran/Form1.Designer.vb
@@ -27,60 +27,77 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TbUser
         '
-        Me.TbUser.Location = New System.Drawing.Point(439, 104)
+        Me.TbUser.Location = New System.Drawing.Point(658, 160)
+        Me.TbUser.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TbUser.Name = "TbUser"
-        Me.TbUser.Size = New System.Drawing.Size(130, 20)
+        Me.TbUser.Size = New System.Drawing.Size(193, 26)
         Me.TbUser.TabIndex = 0
         '
         'TbPassword
         '
-        Me.TbPassword.Location = New System.Drawing.Point(439, 143)
+        Me.TbPassword.Location = New System.Drawing.Point(658, 220)
+        Me.TbPassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.TbPassword.Name = "TbPassword"
         Me.TbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TbPassword.Size = New System.Drawing.Size(130, 20)
+        Me.TbPassword.Size = New System.Drawing.Size(193, 26)
         Me.TbPassword.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(355, 104)
+        Me.Label1.Location = New System.Drawing.Point(532, 160)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.Size = New System.Drawing.Size(83, 20)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Username"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(355, 143)
+        Me.Label2.Location = New System.Drawing.Point(532, 220)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Password"
         '
         'BtnLogin
         '
-        Me.BtnLogin.Location = New System.Drawing.Point(460, 199)
+        Me.BtnLogin.Location = New System.Drawing.Point(690, 306)
+        Me.BtnLogin.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BtnLogin.Name = "BtnLogin"
-        Me.BtnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLogin.Size = New System.Drawing.Size(112, 35)
         Me.BtnLogin.TabIndex = 4
         Me.BtnLogin.Text = "Log In"
         Me.BtnLogin.UseVisualStyleBackColor = True
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(156, 225)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.BtnLogin)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TbPassword)
         Me.Controls.Add(Me.TbUser)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -93,4 +110,5 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents BtnLogin As Button
+    Friend WithEvents Label3 As Label
 End Class
