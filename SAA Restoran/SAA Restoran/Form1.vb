bef8cfe2a8e1dd2ab7a41da035143b4ab2395bb4
allexlim <57930167+allexlim@users.noreply.github.com>
2020-04-30 09:13:09 +0800

    Log In masuk form2

--- SAA Restoran/SAA Restoran/Form1.vb
+++ SAA Restoran/SAA Restoran/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
+        Form2.ShowDialog()
+    End Sub
 End Class
